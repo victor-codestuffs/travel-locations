a72491f563e63d25590e8a43faf9c2ec890f0ea0
Victor Hoang <vic.hoang@yahoo.com>
2019-07-21 22:37:46 -0700

    Update layout, links

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,41 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import '../styles/index.sass';
+import '../styles/main.sass';
 
 main();
 
 function main() {
-  console.info("hello world");
+  let selectedContent = "one";
+
+  const nav = document.querySelector(".Nav");
+  const links = document.querySelectorAll(".Nav-listItemLink");
+  const sections = document.querySelectorAll(".Main-content");
+
+  nav.addEventListener("click", ev => {
+    // If link is clicked
+    if (ev.target && ev.target.classList.contains("Nav-listItemLink") ) {
+      ev.preventDefault();
+
+      const targetSection = ev.target.dataset.select;
+      if (targetSection === selectedContent) return;
+      selectedContent = targetSection;
+
+      links.forEach(link => {
+        if (link === ev.target) {
+          ev.target.classList.add("is-selected");
+        } else {
+          link.classList.remove("is-selected");
+        }
+      });
+
+      sections.forEach(section => {
+        if (section.dataset.section === selectedContent) {
+          section.classList.add("is-active");
+        } else {
+          section.classList.remove("is-active");
+        }
+      });
+    }
+  });
 }
